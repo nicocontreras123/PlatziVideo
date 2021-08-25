8672b05935fde8651574fed2d8062b0dd8c21847
Nicolas Contreras <n.contrerasorellana@gmail.com>
2021-08-25 01:03:32 -0400

    EsLint

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import HelloWorld from './componets/HelloWorld'
+ReactDom.render(<HelloWorld></HelloWorld>, document.getElementById('app'))
