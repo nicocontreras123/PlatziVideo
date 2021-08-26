acd9b5079b38b8e082367ad1e5ad94b3ef8da240
Nicolás Contreras <31162897+nicocontreras123@users.noreply.github.com>
2021-08-25 20:33:57 -0400

    Clase18

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,13 +4,14 @@ import Search from '../componets/Search';
 import Categories from '../componets/Categories';
 import Carousel from '../componets/Carousel';
 import CarouselItem from '../componets/CarouselItem';
+import Footer from '../componets/Footer';
 import '../assets/styles/App.scss';
 
 const App = () => (
   <div className='App'>
     <Header />
     <Search />
-    <Categories>
+    <Categories title='En busqueda'>
       <Carousel>
         <CarouselItem />
         <CarouselItem />
@@ -18,6 +19,23 @@ const App = () => (
         <CarouselItem />
       </Carousel>
     </Categories>
+    <Categories title='Comedia'>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+    <Categories title='Drama'>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+    <Footer />
   </div>
 );
 
